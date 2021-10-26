3f602ffa7c6d21a9e07fa98843cb15521bb13a90
yocosaka <sakayoco55@gmail.com>
2021-10-26 15:04:47 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users
+      resources :chatrooms do
+        resources :messages
+      end
+      # resources :login, only: %i[create]
+      # resources :auto_login, only: %i[create]
+    end
+  end
 end
