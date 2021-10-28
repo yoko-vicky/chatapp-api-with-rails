e70da987b6aac00f61d2318b514e1e37f074182d
yocosaka <sakayoco55@gmail.com>
2021-10-28 13:05:25 +0900

    Add cors setting in config/initializers/cors.rb without exposing access token

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,23 +1,23 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   # for development
-#   allow do
-#     origins 'http://localhost:3000'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # for development
+  allow do
+    origins 'http://localhost:3000'
 
-#     resource '*',
-#              headers: :any,
-#              expose: %w[access-token id],
-#              methods: %i[get post put patch delete options head],
-#              credentials: true
-#   end
+    resource '*',
+             headers: :any,
+             #  expose: %w[access-token id],
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
 
-#   # for production
-#   allow do
-#     origins 'https://chat-app-api-dream-mv.herokuapp.com/'
+  # for production
+  allow do
+    origins 'https://chat-app-api-dream-mv.herokuapp.com/'
 
-#     resource '*',
-#              headers: :any,
-#              expose: %w[access-token id],
-#              methods: %i[get post put patch delete options head],
-#              credentials: true
-#   end
-# end
+    resource '*',
+             headers: :any,
+             #  expose: %w[access-token id],
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+end
